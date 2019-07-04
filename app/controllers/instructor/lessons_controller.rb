84a5a6953ab5c4a086a5ffff5938c9f762d3442a
cw2981 <cw2981>
2019-07-04 22:46:07 +0000

    having trouble pushing heroku attempting to fix the error.

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,17 +1,30 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+
+
   def new
-    @section = Section.find(params[:section_id])
+    @section = current_section
+    if @section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
     @lesson = Lesson.new
   end
 
   def create 
-    @section = Section.find(params[:section_id])
+    @section = current_section 
+      if @section.course.user != current_user
+        return render plain: 'Unauthorized', status :unauthorized
+      end
     @lesson = @section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@section.course)
   end
 
 
   private
+
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
