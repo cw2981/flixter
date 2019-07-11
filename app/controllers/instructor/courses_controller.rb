a3743158f9d08d0eb82c5af953c26d3c3baac898
cw2981 <cw2981>
2019-07-11 23:23:50 +0000

    attempting to fix heroku push error

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,32 +6,32 @@ class Instructor::CoursesController < ApplicationController
     @course = Course.new
   end
 
-def create 
-  @course = current_user.courses.create(course_params)
-  if @course.valid?
-  redirect_to instructor_course_path(@course)
-else
-  render :new, status: :unprocessable_entity
+  def create 
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+    redirect_to instructor_course_path(@course)
+  else
+    render :new, status: :unprocessable_entity
+    end
   end
-end
 
-def show
-end
+  def show
+  end
 
 private
 
-def require_authorized_for_current_course
-  if current_course.user != current_user
-    render plain: "Unauthorized", status: :unauthorized 
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized 
+    end
   end
-end
 
   helper_method :current_course
   def current_course
     @current_course ||= Course.find(params)[:id]
   end
 
-def course_params
-  params.require(:course).permit(:title, :description, :cost)
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+    end
   end
-end
