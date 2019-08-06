8f7acd10428904463ecce57d5859bd1340223270
cw2981 <cw2981>
2019-08-06 15:36:48 +0000

    modal added for sections lesson 35 complete

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,11 +1,7 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
-
-  def new
-    @lesson = Lesson.new
-  end
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
@@ -19,12 +15,6 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
@@ -34,7 +24,13 @@ class Instructor::LessonsController < ApplicationController
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
-
+ 
+ def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+ 
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
