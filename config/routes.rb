db7152898fa3d5f47b4fd4c98a22e1a6fe857b37
cw2981 <cw2981>
2019-08-06 23:22:16 +0000

    fixed javascript error, routing error still present.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
-    root 'static_pages#index'
-    get 'privacy', to: 'static_pages#privacy'
+  root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :sections, only: [:update] do
       resources :lessons, only: [:create]
     end
-   resources :courses, only: [:new, :create, :show] do
+    resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
   end
